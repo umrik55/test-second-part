7bb463acf58a31fadad2158dbc6962c172fdf650
umrik <umrik@ukr.net>
2020-10-21 16:03:12 +0300

    bd 193.23.225.174 port 2701 autent 6

--- routes/eshlttcform.js
+++ routes/eshlttcform.js
@@ -116,6 +116,7 @@ module.exports = app => {
 		var result1 = await db.collection('fuel').find({ _id: name }).toArray();
 		//console.log(JSON.parse(result1[0]).length);
 		//console.log(result1[0]);
+		
 		return result1[0].tempData.cont;		
 		}catch(e){			
 			console.log("Помилка читання БД "+name);
@@ -137,7 +138,7 @@ module.exports = app => {
 		try{
 		var result1 = await db.collection('evends').find({ _id: name }).toArray();
 		//console.log(JSON.parse(result1[0].tempData.cont).length);
-		console.log(result1[0].tempData.cont);
+		console.log(result1[0].tempData.cont[0]);
 		console.log(Object.keys(result1[0].tempData.cont));
 		return result1[0].tempData.cont;
 		}catch(e){
