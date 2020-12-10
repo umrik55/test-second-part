c69f424d6d1166f9311e214022e7ba8664c09fb0
umrik <umrik@ukr.net>
2020-12-10 11:47:37 +0200

    finish servis Equips Note_time 1

--- routes/agent.js
+++ routes/agent.js
@@ -1465,8 +1465,10 @@ if (key1==="validCount" || key1==="timestamp"){
 		var filter_date_start = req.body.bodyreg.filter.date.start;
 		var filter_date_end = req.body.bodyreg.filter.date.end;
 		
-		//var filter_Note_time = req.body.bodyreg.filter.Note_time;
-		var filter_Note_time = filter_date_start+"T" + req.body.bodyreg.filter.Note_time;
+		var filter_Note_time = req.body.bodyreg.filter.Note_time;
+		if(filter_Note_time!=""){
+			filter_Note_time = filter_date_start+"T" + req.body.bodyreg.filter.Note_time;
+		}
 		var filter_Location_type = req.body.bodyreg.filter.Location_type;
 		var filter_Event = req.body.bodyreg.filter.Event;		
 		var filter_Device_ID = req.body.bodyreg.filter.Device_ID;
